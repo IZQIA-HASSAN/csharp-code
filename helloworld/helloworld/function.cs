fe48807f07cb393ee8d2b2fbf175083274cb12d0
ls <izqiaulhassan166@gmail.com>
2026-06-13 22:19:44 +0500

    functions in c-sharp

---
+++ helloworld/helloworld/function.cs
@@ -0,0 +1,11 @@
+class Program
+{
+    static void MyMethod(string name , int age )
+    {
+      Console.WriteLine("My name is " + name + " and my age is " + age);  
+    }
+    static void Main(string[] args)
+    {
+        MyMethod("izqia" , 4);
+    }
+}
--- helloworld/helloworld/obj/Debug/net10.0/helloworld.AssemblyInfo.cs
+++ helloworld/helloworld/obj/Debug/net10.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b256edb5600d72db74881cd30dcb4b772108e02d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7fa75aa074c4892bb129ff945ef7ffb1d766564")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
